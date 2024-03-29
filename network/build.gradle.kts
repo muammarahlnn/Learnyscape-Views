b8a06dac146e58cb5bd5bde7954feccafc53faef
muammarahlnn <21muammarahlan@gmail.com>
2024-03-29 16:31:15 +0800

    feat(network): add network module

---
+++ network/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("lsv.android.library")
+    id("lsv.android.hilt")
+    id("kotlinx-serialization")
+}
+
+val BASE_URL: String by project
+
+android {
+    namespace = "com.muammarahlnn.lsv.network"
+    buildFeatures {
+        buildConfig = true
+    }
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+    defaultConfig {
+        buildConfigField("String", "BASE_URL", BASE_URL)
+    }
+}
+
+dependencies {
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.okhttp.logging)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+}
