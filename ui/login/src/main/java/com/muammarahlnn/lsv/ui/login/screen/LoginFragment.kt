36145dce3bf27e6ca7947fb234a6bf94171247b5
muammarahlnn <21muammarahlan@gmail.com>
2024-04-30 18:30:55 +0800

    feat(login): disabled login button when user doesn't fill the fields

--- ui/login/src/main/java/com/muammarahlnn/lsv/ui/login/screen/LoginFragment.kt
+++ ui/login/src/main/java/com/muammarahlnn/lsv/ui/login/screen/LoginFragment.kt
@@ -5,7 +5,11 @@ import android.view.LayoutInflater
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.muammarahlnn.lsv.core.navigation.navigateToHomeNavigator
 import com.muammarahlnn.lsv.core.ui.ext.hideSoftKeyboard
 import com.muammarahlnn.lsv.core.ui.ext.readDrawable
@@ -13,6 +17,10 @@ import com.muammarahlnn.lsv.core.ui.fragment.BaseFragment
 import com.muammarahlnn.lsv.ui.login.R
 import com.muammarahlnn.lsv.ui.login.databinding.ScreenLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 /**
  * @Author Muammar Ahlan Abimanyu
@@ -20,6 +28,10 @@ import dagger.hilt.android.AndroidEntryPoint
  */
 @AndroidEntryPoint
 internal class LoginFragment : BaseFragment<ScreenLoginBinding, LoginViewModel, LoginUiState>() {
+
+    private val usernameFlow = MutableStateFlow("")
+
+    private val passwordFLow = MutableStateFlow("")
 
     override val viewModel: LoginViewModel by viewModels()
 
@@ -29,6 +41,7 @@ internal class LoginFragment : BaseFragment<ScreenLoginBinding, LoginViewModel, 
 
     override suspend fun onViewLoaded(savedInstanceState: Bundle?) {
         setupView()
+        observeLoginButtonEnabledState()
     }
 
     override fun renderState(state: LoginUiState) {
@@ -41,13 +54,20 @@ internal class LoginFragment : BaseFragment<ScreenLoginBinding, LoginViewModel, 
     }
 
     private fun setupView() {
+        viewBinding.etUsername.addTextChangedListener { text ->
+            usernameFlow.update { text.toString() }
+        }
+        viewBinding.etPassword.addTextChangedListener { text ->
+            passwordFLow.update { text.toString() }
+        }
+
         viewBinding.btnLogin.also { button ->
             button.setOnClickListener {
                 it.hideSoftKeyboard()
 
                 viewModel.userLogin(
-                    username = viewBinding.etUsername.text.toString(),
-                    password = viewBinding.etPassword.text.toString()
+                    username = usernameFlow.value,
+                    password = passwordFLow.value,
                 )
             }
         }
@@ -79,6 +99,22 @@ internal class LoginFragment : BaseFragment<ScreenLoginBinding, LoginViewModel, 
 
             btnLoginLoading.visibility = GONE
             btnLoginText.visibility = VISIBLE
+        }
+    }
+
+    private fun observeLoginButtonEnabledState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                combine(usernameFlow, passwordFLow) { username, password ->
+                    username.isNotBlank() && password.isNotBlank()
+                }.collect { isLoginButtonEnabled ->
+                    viewBinding.btnLogin.isEnabled = isLoginButtonEnabled
+                    viewBinding.btnLogin.background = readDrawable(
+                        if (isLoginButtonEnabled) R.drawable.bg_login_button_enabled
+                        else R.drawable.bg_login_button_disabled
+                    )
+                }
+            }
         }
     }
 }
