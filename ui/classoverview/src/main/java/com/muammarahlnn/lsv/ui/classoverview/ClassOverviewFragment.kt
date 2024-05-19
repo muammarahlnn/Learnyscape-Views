51bc5e61b88f09f8c5b4689f877580384669b942
muammarahlnn <21muammarahlan@gmail.com>
2024-05-19 19:23:28 +0800

    feat(classoverview): add class overview screen ui

--- ui/classoverview/src/main/java/com/muammarahlnn/lsv/ui/classoverview/ClassOverviewFragment.kt
+++ ui/classoverview/src/main/java/com/muammarahlnn/lsv/ui/classoverview/ClassOverviewFragment.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import com.muammarahlnn.lsv.core.navigation.CLASS_ID_ARG
+import com.muammarahlnn.lsv.core.navigation.ClassOverviewScreen
+import com.muammarahlnn.lsv.core.navigation.OVERVIEW_SCREEN_ARG
+import com.muammarahlnn.lsv.core.navigation.getRootNavController
+import com.muammarahlnn.lsv.core.ui.ext.readText
 import com.muammarahlnn.lsv.core.ui.fragment.BaseFragment
 import com.muammarahlnn.lsv.ui.classoverview.databinding.ScreenClassOverviewBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -33,5 +38,24 @@ internal class ClassOverviewFragment :
     }
 
     private fun setupView() {
+        val classId = arguments?.getString(CLASS_ID_ARG) as String
+        val overviewScreenOrdinal = arguments?.getInt(OVERVIEW_SCREEN_ARG) as Int
+
+        viewBinding.toolbar.ivBack.also { view ->
+            view.setOnClickListener {
+                requireActivity().getRootNavController().navigateUp()
+            }
+        }
+
+        viewBinding.toolbar.tvTitle.also { view ->
+            view.text = readText(
+                when (overviewScreenOrdinal) {
+                    ClassOverviewScreen.MODULE.ordinal -> R.string.module
+                    ClassOverviewScreen.ASSIGNMENT.ordinal -> R.string.assignment
+                    ClassOverviewScreen.QUIZ.ordinal -> R.string.quiz
+                    else -> throw IllegalArgumentException("Overview screen ordinal not found")
+                }
+            )
+        }
     }
 }
