e406084aae3f67ca8aabdc35704b5501be8bcdf9
muammarahlnn <21muammarahlan@gmail.com>
2024-05-22 17:30:40 +0800

    feat(resourcedetails): add resource details module

---
+++ ui/resourcedetails/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("lsv.android.ui")
+}
+
+android {
+    namespace = "com.muammarahlnn.lsv.ui.resourcedetails"
+}
+
+dependencies {
+    implementation(libs.material)
+    implementation(libs.kotlinx.datetime)
+}
